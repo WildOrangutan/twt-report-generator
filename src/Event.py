8e2bfa796ec5594825e637bf81ac2eb9670630ff
Peter Mandeljc <WildOrangutan@users.noreply.github.com>
2020-02-12 20:53:13 +0100

    Changed Event parsing methods to static

--- src/Event.py
+++ src/Event.py
@@ -11,19 +11,21 @@ class Event:
     @classmethod
     def parse(self, string):
         # Example string: 2020-01-06 08:18;in;Default;
-        values = self.__splitString(self, string)
-        dateTime = self.__parseDate(self, values[0])
+        values = self.__splitString(string)
+        dateTime = self.__parseDate(values[0])
         eventType = EventType.parse(values[1])
         return self(datetime=dateTime, eventType=eventType)
 
-    def __splitString(self, string):
+    @staticmethod
+    def __splitString(string):
         values = string.split(';')
         length = len(values)
         if length==0 or length < 2:
             raise ParseError(f"Expected at least 2 string parts, found {length}")
         return values
 
-    def __parseDate(self, string):
+    @staticmethod
+    def __parseDate(string):
         dateTimeFormat = "%Y-%m-%d %H:%M"
         return datetime.strptime(string, dateTimeFormat)
 
