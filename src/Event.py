0b81acae73d299d6bd027f1e697e57c6c74d314d
Peter Mandeljc <WildOrangutan@users.noreply.github.com>
2020-02-12 20:40:22 +0100

    Changed Event to check split string

--- src/Event.py
+++ src/Event.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from dataclasses import dataclass
 from src.EventType import EventType
+from src.ParseError import ParseError
 
 @dataclass
 class Event:
@@ -10,10 +11,17 @@ class Event:
     @classmethod
     def parse(self, string):
         # 2020-01-06 08:18;in;Default;
-        values = string.split(';')
+        values = self.__splitString(self, string)
         dateTime = self.__parseDate(self, values[0])
         eventType = EventType.parse(values[1])
         return self(datetime=dateTime, eventType=eventType)
+
+    def __splitString(self, string):
+        values = string.split(';')
+        length = len(values)
+        if length==0 or length < 2:
+            raise ParseError(f"Expected at least 2 string parts, found {length}")
+        return values
 
     def __parseDate(self, string):
         dateTimeFormat = "%Y-%m-%d %H:%M"
