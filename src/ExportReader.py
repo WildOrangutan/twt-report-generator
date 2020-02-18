cf73395dc3d1826e5b2176176ef85f21faad5505
Peter Mandeljc <WildOrangutan@users.noreply.github.com>
2020-02-18 20:21:45 +0100

    Changed ExportReader to use EventParser

--- src/ExportReader.py
+++ src/ExportReader.py
@@ -1,16 +1,21 @@
 from typing import Iterable
 from src.Event import Event
+from src.EventParser import EventParser
 
 class ExportReader:
 
     def __init__(self, fileLineIterator: Iterable[str]):
         self.fileLineIterator = fileLineIterator
-        next(fileLineIterator) # Skip header
+        self.__initEventParser()
+    
+    def __initEventParser(self):
+        headerLine = next(self.fileLineIterator)
+        self.eventParser = EventParser(headerLine)
 
     def __iter__(self):
         return self
 
     def __next__(self):
         line = next(self.fileLineIterator)
-        event = Event.parse(line)
+        event = self.eventParser.parse(line)
         return event
