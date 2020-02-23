4df7fea93be8b4990db4465f068d5c98a338955c
Peter Mandeljc <WildOrangutan@users.noreply.github.com>
2020-02-24 00:32:24 +0100

    Changed EventType to extend ParsableEnum

--- src/EventType.py
+++ src/EventType.py
@@ -1,15 +1,7 @@
-from enum import Enum
+from src.ParsableEnum import ParsableEnum
 from src.ParseError import ParseError
 
 
-class EventType(Enum):
+class EventType(ParsableEnum):
     IN = "in"
     OUT = "out"
-
-    @classmethod
-    def parse(cls, string):
-        try:
-            cleanString = string.strip()
-            return cls(cleanString)
-        except Exception as e:
-            raise ParseError(f"Failed to parse from '{cleanString}'") from e
